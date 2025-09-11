ad36fae1c63f781c43bdea059bc9d8fe50982897
Antor Web-Dev <antorremeni@gmail.com>
2025-09-11 21:51:59 +0600

    change

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,53 +9,10 @@ const Banner = () => {
     <>
     
 
-    <section style={{ background: `URL(${bannerBg})`,  backgroundRepeat: `no-repeat`,  backgroundSize: `cover`, }}  id='banner'>
+    <section style={{ background: `URL(${bannerBg})`,  backgroundRepeat: `no-repeat`,  backgroundSize: `cover`, }}  id='banner' className='pb-[555px] relative'>
 
 
-      <div className='w-[1064px] bg-[#fff] rounded-[4px] shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)]'>
-
-        <div>
-
-          <div>
-            <h3>Flying From</h3>
-                       <input type="text" placeholder='City or Airport'/>
-
-          </div>
-                <div>
-            <h3>Flying To</h3>
-                        <input type="text" placeholder='City or Airport'/>
-
-          </div>
-
-      <div>
-            <h3>Depart</h3>
-            <input type=" date" placeholder='Mon, 14/03'/>
-           
-          </div>
-
-      <div>
-            <h3>Return</h3>
-            <input type=" date" placeholder='Mon, 14/03'/>
-           
-          </div>
-
-      <div>
-            <h3>Travellers</h3>
-                        <input type="number" placeholder='5 Travellers'/>
-
-          </div>
-
-         <button className='w-[148px] bg-[#13357B]'><LuSearch /> Search</button>
- 
-
-
-
-
-        </div>
-
-
-
-      </div>
+   
 
 
 
@@ -69,18 +26,68 @@ const Banner = () => {
 
 
               {/* ------------ left-side */}
-              <h1>Where do You Want To Fly</h1>
-              <p>Discover amzaing places at exclusive deals</p>
+              <h1 className='text-[60px] font-jost font-semibold text-main'>Where do You Want To <span className='text-[#13357B]'>Fly</span></h1>
+              <p className='text-[16px] font-normal font-jost text-[#697488]'>Discover amzaing places at exclusive deals</p>
 
-              <div>
+              <div className='flex items-center gap-[30px] mt-[29px] mb-[19px]'>
 
-                <Link to={'/'} >Return <IoMdArrowDropdown /></Link>
-                <Link to={'/'} >Economy <IoMdArrowDropdown /></Link>
-                <Link to={'/'} >0 Bags <IoMdArrowDropdown /></Link>
+                <Link to={'/'} className='flex items-center gap-[12px]' >Return <IoMdArrowDropdown /></Link>
+                <Link to={'/'} className='flex items-center gap-[12px]' >Economy <IoMdArrowDropdown /></Link>
+                <Link to={'/'} className='flex items-center gap-[12px]' >0 Bags <IoMdArrowDropdown /></Link>
           
               </div>
 
-                
+                   <div className='w-[1144px] bg-[#fff] rounded-[4px] flex shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)]'>
+
+        <div className='flex mt-[27px] mb-[23px] px-[20px]'>
+
+          <div  className='flex'>
+
+
+          <div className='pr-[70px] border-r h-[40px] ' >
+            <h3 className='text-[15px] font-medium font-jost text-main'>Flying From</h3>
+                       <input className='text-[15px]  border-none outline-none font-normal font-jost text-[#697488]' type="text" placeholder='City or Airport'/>
+
+          </div>
+                <div className='pl-[30px] pr-[70px] border-r h-[40px]'>
+            <h3 className='text-[15px] font-medium font-jost text-main'>Flying To</h3>
+                        <input className='text-[15px] border-none outline-none  font-normal font-jost text-[#697488]' type="text" placeholder='City or Airport'/>
+
+          </div>
+
+      <div className='pl-[30px]'>
+            <h3 className='text-[15px] font-medium font-jost text-main'>Depart</h3>
+            <input className='text-[15px] font-normal border-none outline-none  font-jost text-[#697488]' type=" date" placeholder='Mon, 14/03'/>
+           
+          </div>
+
+      <div className=' pl-[20px] border-l border-r '>
+            <h3 className='text-[15px] font-medium font-jost text-main'>Return</h3>
+            <input className='text-[15px] font-normal border-none outline-none  font-jost text-[#697488]' type=" date" placeholder='Mon, 14/03'/>
+           
+          </div>
+
+      <div className='pl-[20px]'>
+            <h3 className='text-[15px] font-medium font-jost text-main'>Travellers</h3>
+                        <input className='text-[15px] border-none outline-none  font-normal font-jost text-[#697488]' type="number" placeholder='5 Travellers'/>
+
+          </div>
+
+
+          </div>
+
+
+         <button className='w-[148px] text-[15px] font-medium font-jost text-[#fff] flex justify-center rounded-[4px] items-center gap-[10px] bg-[#13357B]'><LuSearch /> Search</button>
+ 
+
+
+        </div>
+
+
+
+
+
+      </div>
             </div>
         </div>
     </section>
