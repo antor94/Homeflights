af2315ceb22087601528be16d9698119dc365ea1
Antor Web-Dev <antorremeni@gmail.com>
2025-09-11 21:04:30 +0600

    chnage

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import bannerBg from '../assets/images/bannerBg.png'
+import bannerBg from '../assets/images/SLIDER.png'
+import { Link } from 'react-router'
+import { IoMdArrowDropdown } from "react-icons/io";
+import { LuSearch } from "react-icons/lu";
 
 const Banner = () => {
   return (
@@ -7,8 +10,75 @@ const Banner = () => {
     
 
     <section style={{ background: `URL(${bannerBg})`,  backgroundRepeat: `no-repeat`,  backgroundSize: `cover`, }}  id='banner'>
+
+
+      <div className='w-[1064px] bg-[#fff] rounded-[4px] shadow-[0px_4px_30px_0px_rgba(31,_38,_135,_0.15)]'>
+
+        <div>
+
+          <div>
+            <h3>Flying From</h3>
+                       <input type="text" placeholder='City or Airport'/>
+
+          </div>
+                <div>
+            <h3>Flying To</h3>
+                        <input type="text" placeholder='City or Airport'/>
+
+          </div>
+
+      <div>
+            <h3>Depart</h3>
+            <input type=" date" placeholder='Mon, 14/03'/>
+           
+          </div>
+
+      <div>
+            <h3>Return</h3>
+            <input type=" date" placeholder='Mon, 14/03'/>
+           
+          </div>
+
+      <div>
+            <h3>Travellers</h3>
+                        <input type="number" placeholder='5 Travellers'/>
+
+          </div>
+
+         <button className='w-[148px] bg-[#13357B]'><LuSearch /> Search</button>
+ 
+
+
+
+
+        </div>
+
+
+
+      </div>
+
+
+
+
         <div className="container">
+
+
+
+
             <div id='banner-row'>
+
+
+              {/* ------------ left-side */}
+              <h1>Where do You Want To Fly</h1>
+              <p>Discover amzaing places at exclusive deals</p>
+
+              <div>
+
+                <Link to={'/'} >Return <IoMdArrowDropdown /></Link>
+                <Link to={'/'} >Economy <IoMdArrowDropdown /></Link>
+                <Link to={'/'} >0 Bags <IoMdArrowDropdown /></Link>
+          
+              </div>
 
                 
             </div>
